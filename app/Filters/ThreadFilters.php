e61573b8cf21c47a034dc145344b334db2eea7ba
mt1sk <mt1sk@mail.ru>
2019-11-13 10:03:31 +0200

    sort thread by popular(replies_count).

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class ThreadFilters extends Filters
 {
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
     /**
      * @param $username
      * @return mixed
@@ -18,5 +18,11 @@ class ThreadFilters extends Filters
     {
         $user = User::where('name', $username)->firstOrFail();
         return $this->builder->where('user_id', $user->id);
+    }
+
+    protected function popular()
+    {
+        $this->builder->getQuery()->orders = [];
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        body {padding-bottom: 100px;}
+        .level {
+            display: flex;
+            align-items: center;
+        }
+        .flex {flex: 1;}
+    </style>
 </head>
-<body style="padding-bottom: 100px;">
+<body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -43,6 +52,7 @@
                                 @auth
                                     <a class="dropdown-item" href="{{ route('threads.index', ['by' => auth()->user()->name]) }}">My threads</a>
                                 @endauth
+                                <a class="dropdown-item" href="{{ route('threads.index', ['popular' => 1]) }}">Popular threads</a>
                             </div>
                         </li>
 
